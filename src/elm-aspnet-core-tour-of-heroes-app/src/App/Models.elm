50487e990de578320fa482302a58924a1136d4c4
Henrik Andersson <henrik@henrikandersson.org>
2017-03-25 10:36:46 +1000

    Part 2: Master / Detail

--- src/elm-aspnet-core-tour-of-heroes-app/src/App/Models.elm
+++ src/elm-aspnet-core-tour-of-heroes-app/src/App/Models.elm
@@ -2,17 +2,33 @@ module App.Models exposing (..)
 
 type alias Model =
     { title: String
-    , hero: Hero 
+    , heroes: List Hero
+    , selectedHero: Maybe Hero 
     }
+
+heroes : List Hero
+heroes =
+    [
+        Hero 11 "Mr. Nice" False,
+        Hero 12 "Narco" False,
+        Hero 13 "Bombasto" False,
+        Hero 14 "Celeritas" False,
+        Hero 15 "Magneta" False,
+        Hero 16 "RubberMan" False,
+        Hero 17 "Dynama" False,
+        Hero 18 "Dr IQ" False,
+        Hero 19 "Magma" False,
+        Hero 20 "Tornado" False
+    ]
 
 initialModel : Model
 initialModel =
     { title = "Tour of Heroes"
-    , hero = 
-        { id = 1
-        , name = "Windstorm" }
+    , heroes = heroes
+    , selectedHero = Nothing
     }
 
 type alias Hero =
     { id: Int
-    , name: String }
+    , name: String
+    , isSelected: Bool }
--- src/elm-aspnet-core-tour-of-heroes-app/src/App/View.elm
+++ src/elm-aspnet-core-tour-of-heroes-app/src/App/View.elm
@@ -1,27 +1,63 @@
 module App.View exposing (..)
 
 import Html exposing (..) 
-import Html.Attributes exposing (value)
-import Html.Events exposing (onInput, targetValue)
+import Html.Attributes exposing (class, value)
+import Html.Events exposing (onInput, targetValue, onClick)
 
 import App.Messages exposing (Msg(..))
-import App.Models exposing (Model)
+import App.Models exposing (Model, Hero)
 
 view : Model -> Html Msg
 view model =
     div []
         [ h1 [] [ text model.title ]
-        , h2 [] [ text model.hero.name, text " details!" ]
-        , div [] 
-            [ label [] [ text "id: " ] 
-            , text (toString model.hero.id) ]
-        , div []
-            [ label [] [ text "name: " ]
-            , input 
-                [ value model.hero.name
-                , onInput ChangeName
-                ] 
-                [] 
-            ]
+        , heroesList model.heroes
+        , selectedHero model.selectedHero
         ]
 
+heroesList : (List Hero) -> Html Msg
+heroesList heroes =
+    div []
+        [ h2 [] [ text "My Heroes" ]
+        , ul [ class "heroes" ]
+            (List.map heroRow heroes)
+        ] 
+
+heroRow : Hero -> Html Msg
+heroRow hero =
+    let
+        getClass isSelected =
+            if isSelected then
+                "selected"
+            else
+                ""
+
+        selectHeroMessage =
+            SelectHero hero    
+    in
+        li [ class (getClass hero.isSelected), onClick selectHeroMessage ]
+            [ span [ class "badge" ] [ text (toString hero.id) ]
+            , text hero.name
+            ]
+
+selectedHero : Maybe Hero -> Html Msg
+selectedHero hero =
+    case hero of
+        Nothing -> 
+            text ""
+
+        Just hero ->
+            div []
+                [ h2 [] [ text hero.name, text " details!" ]
+                , div [] 
+                    [ label [] [ text "id: " ] 
+                    , text (toString hero.id) ]
+                , div []
+                    [ label [] [ text "name: " ]
+                    , input 
+                        [ value hero.name
+                        , onInput ChangeName
+                        ] 
+                        [] 
+                    ]
+                ]
